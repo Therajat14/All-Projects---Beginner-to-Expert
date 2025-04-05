814b9807952c7c07fdaa3a8bf00c334468db497f
therajat14 <heylegend14@gmail.com>
2025-04-06 00:57:10 +0530

    Improve user validation utility

--- 01-Beginner-Level/01-User-Authentication-System/backend/utils/validation/userValidation.js
+++ 01-Beginner-Level/01-User-Authentication-System/backend/utils/validation/userValidation.js
@@ -16,7 +16,6 @@ const userLogInValidation = z.object({
   email: z.string().email({ message: "Invalid email address" }),
   password: z
     .string()
-    .min(6, { message: "Password must be at least 6 characters" }),
 });
 
 module.exports = { userSignUpValidation, userLogInValidation };
