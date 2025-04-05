c675de224d860be4b2e6dc0445463301fc676dce
therajat14 <heylegend14@gmail.com>
2025-04-06 00:57:10 +0530

    Update JWT utility functions

--- 01-Beginner-Level/01-User-Authentication-System/backend/utils/jwt.js
+++ 01-Beginner-Level/01-User-Authentication-System/backend/utils/jwt.js
@@ -2,8 +2,20 @@ const jwt = require("jsonwebtoken");
 
 const token = (payload) => {
   return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: 60,
+    expiresIn: "2m",
   });
 };
 
-module.exports = token;
+const authenticateToken = (token) => {
+
+
+  if (!token) return {success : false,  message: "No token provided" };
+
+  return jwt.verify(token,  process.env.JWT_SECRET, (err, user) => {
+    if (err) return { success : false, message: "Token is not valid" };
+    
+   else { user.success = true;
+    return user;}
+  });
+};
+module.exports = {token, authenticateToken}
