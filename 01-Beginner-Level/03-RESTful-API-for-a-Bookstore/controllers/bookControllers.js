0710270c76487ba9b752fdf6229175d785a62dc4
therajat14 <heylegend14@gmail.com>
2025-05-27 03:15:18 +0530

    Finally created POST books/ endpoint

--- 01-Beginner-Level/03-RESTful-API-for-a-Bookstore/controllers/bookControllers.js
+++ 01-Beginner-Level/03-RESTful-API-for-a-Bookstore/controllers/bookControllers.js
@@ -1,5 +1,14 @@
-const getAllBooks = (req, res) => {
-  res.send("hi 6there");
+import Book from "../models/bookModel.js";
+
+const getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.find().lean();
+    res.status(200).json({ books });
+  } catch (err) {
+    res.status(400).json({
+      Error: err.message,
+    });
+  }
 };
 
 export default getAllBooks;
---
+++ 01-Beginner-Level/03-RESTful-API-for-a-Bookstore/models/bookModel.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Book title is required"],
+      trim: true,
+    },
+    author: {
+      type: String,
+      required: [true, "Author is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+    },
+    genre: {
+      type: String,
+      required: true,
+      enum: [
+        "Fiction",
+        "Non-Fiction",
+        "Fantasy",
+        "Science",
+        "Biography",
+        "Other",
+      ],
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: [0, "Price must be a positive number"],
+    },
+    stock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    image: {
+      type: String,
+      default: "", // URL to the cover image
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      default: 0,
+    },
+    reviews: [
+      {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        name: { type: String, required: true },
+        rating: { type: Number, required: true },
+        comment: { type: String },
+      },
+    ],
+    publisher: {
+      type: String,
+    },
+    publishedDate: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+const Book = mongoose.model("Book", bookSchema);
+export default Book;
